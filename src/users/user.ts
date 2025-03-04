58bb1c7746870ce05d6d43e0a415a58fd0e310d7
evepineau <eve.pineau@outlook.fr>
2025-03-04 16:22:08 +0100

    Users' GET routes

--- src/users/user.ts
+++ src/users/user.ts
@@ -7,20 +7,48 @@ export type SendMessageBody = {
   destinationUserId: number;
 };
 
+let lastReceivedMessage: string | null = null;
+let lastSentMessage: string | null = null;
+
 export async function user(userId: number) {
   const _user = express();
   _user.use(express.json());
   _user.use(bodyParser.json());
 
-  // Implémentation de la route /status/
   _user.get("/status", (req, res) => {
-    res.send("live");
+    return res.send("live");
+  });
+
+  _user.get("/getLastReceivedMessage", (req, res) => {
+    return res.json({ result: lastReceivedMessage });
+  });
+
+  _user.get("/getLastSentMessage", (req, res) => {
+    return res.json({ result: lastSentMessage });
+  });
+
+  _user.post("/sendMessage", (req, res) => {
+    const { message } = req.body;
+    if (!message) {
+      return res.status(400).json({ error: "Missing message" });
+    }
+
+    lastSentMessage = message;
+    return res.json({ message: "Message sent successfully" });
+  });
+
+  _user.post("/receiveMessage", (req, res) => {
+    const { message } = req.body;
+    if (!message) {
+      return res.status(400).json({ error: "Missing message" });
+    }
+
+    lastReceivedMessage = message;
+    return res.json({ message: "Message received successfully" });
   });
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
-    console.log(
-      `User ${userId} is listening on port ${BASE_USER_PORT + userId}`
-    );
+    console.log(`User ${userId} is listening on port ${BASE_USER_PORT + userId}`);
   });
 
   return server;
